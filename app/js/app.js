5163c4d61076a7be6becc1b58c31c6e67061e3f0
Damien Pacaud <damien.pacaud@gmail.com>
2013-06-20 01:21:47 +0200

    getting the plumbing to work

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,17 @@
 'use strict';
 
 
-// Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
-  config(['$routeProvider', function($routeProvider) {
+var monPrenom = angular.module('monPrenom', ['monPrenom.filters', 'monPrenom.services', 'monPrenom.directives', 'monPrenom.controllers']);
+
+
+monPrenom.config(['$httpProvider', function($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }
+]);
+
+
+monPrenom.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
     $routeProvider.otherwise({redirectTo: '/view1'});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,9 +2,14 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+angular.module('monPrenom.controllers', []).
+  controller('MyCtrl1', ['$scope','$http',function($scope,$http) {
+  	$scope.testButton = function(){
+  		$http.get('http://localhost:8080/random').success(function(data) {
+		    $scope.result = data;
+		  });
+  		//alert("test");
+  	}
   }])
   .controller('MyCtrl2', [function() {
 
