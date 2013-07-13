dd8013693867a4f2eb865793d645ab9f93b8e055
Damien Pacaud <damien.pacaud@gmail.com>
2013-07-13 18:49:56 +0200

    changin lots of stuff

--- app/js/app.js
+++ app/js/app.js
@@ -12,7 +12,7 @@ monPrenom.config(['$httpProvider', function($httpProvider) {
 
 
 monPrenom.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+    $routeProvider.when('/randomName', {templateUrl: 'partials/random.html', controller: 'RandomNameController'});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.otherwise({templateUrl: 'partials/home.html', controller: 'HomeController'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,14 +3,21 @@
 /* Controllers */
 
 angular.module('monPrenom.controllers', []).
-  controller('MyCtrl1', ['$scope','$http',function($scope,$http) {
-  	$scope.testButton = function(){
-  		$http.get('http://localhost:8080/random').success(function(data) {
-		    $scope.result = data;
-		  });
-  		//alert("test");
-  	}
+  controller('RandomNameController', ['$scope','$http','$location',function($scope,$http,$location) {
+    $http.get('http://localhost:8080/random').success(function(data) {
+        $scope.result = data;
+      });
+
+    $scope.backToHome = function(){
+      //alert("tata");
+      $location.path( "/" );
+    }
   }])
   .controller('MyCtrl2', [function() {
 
+  }])
+  .controller('HomeController', ['$scope','$http','$location',function($scope,$http,$location) {
+    $scope.getRandomName = function(){
+      $location.path( "/randomName");
+    }
   }]);
